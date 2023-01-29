16d84e61fa9666c9342b9629a97b8c53d127c16d
KumaR SonU <122993102+KumarSonu01@users.noreply.github.com>
2023-01-29 18:09:04 +0530

    Add files via upload

---
+++ Check for identity matrix.c
@@ -0,0 +1,30 @@
+// C program to print Identity Matrix
+#include<stdio.h>
+
+int Identity(int num)
+{
+	int row, col;
+	
+	for (row = 0; row < num; row++)
+	{
+		for (col = 0; col < num; col++)
+		{
+			// Checking if row is equal to column
+			if (row == col)
+				printf("%d ", 1);
+			else
+				printf("%d ", 0);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+// Driver Code
+int main()
+{
+	int size = 5;
+	identity(size);
+	return 0;
+}
+
